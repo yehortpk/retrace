e37bc0cd3b9d567428a177214b869dc02e5d4706
yehortpk <e2000work@gmail.com>
2026-09-24 17:35:37 +0000

    Remove StorageProperties in favor of @Value injection
    Config values should be injected directly rather than bound through a
    @ConfigurationProperties class.
    
    - RetraceApplication: drop @ConfigurationPropertiesScan
    - config: delete StorageProperties

--- src/main/java/dev/yetpk/retrace/RetraceApplication.java
+++ src/main/java/dev/yetpk/retrace/RetraceApplication.java
@@ -2,10 +2,8 @@ package dev.yetpk.retrace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan
 public class RetraceApplication {
 
     public static void main(String[] args) {
--- src/main/java/dev/yetpk/retrace/config/StorageProperties.java
+++
@@ -1,7 +0,0 @@
-package dev.yetpk.retrace.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "retrace.storage")
-public record StorageProperties(String root) {
-}
